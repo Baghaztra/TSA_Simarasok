84f517f0ce1fa45306edd9c35bc27af152d8bbac
Baghaztra <127162951+Baghaztra@users.noreply.github.com>
2024-05-17 11:39:40 +0700

    admin

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.layout.main')
+
+@section('content')
+    <h1>Laman dahsboard</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
+});
+Route::get('/admin', function () {
+    return view('admin.dashboard');
 });
